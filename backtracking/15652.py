8b0b4e60f572940ff2a8ace47c601aaa9a2fd8bb
DEVHARAM <gkfka5968@naver.com>
2019-08-01 16:30:48 +0900

    15652-n과 m(4)

---
+++ backtracking/15652.py
@@ -0,0 +1,15 @@
+def back(arr,use,k,start):
+    if k==m: 
+        print(' '.join(map(str,arr)))
+        return
+    for i in range(start,n+1):
+        arr[k]=i
+            
+        back(arr,use,k+1,i)
+       
+            
+    
+n,m=map(int,input().split())
+use=[False for i in range(n+1)]
+arr=[0 for i in range(m)]
+back(arr,use,0,1)
