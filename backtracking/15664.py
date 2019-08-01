88dc239b1ac9c0396d0b80675ae70fe76beebb87
DEVHARAM <gkfka5968@naver.com>
2019-08-01 17:18:47 +0900

    15664-n과 m(10)

---
+++ backtracking/15664.py
@@ -0,0 +1,22 @@
+from collections import deque
+from collections import Counter
+def back(c,start):
+    global result
+    if c==m: 
+        print(' '.join(map(str,q)))
+        return
+    for i in range(start,len(cnt)):
+        if cnt[i]:
+            cnt[i]-=1
+            q.append(arr[i])
+            back(c+1,i)
+            q.pop()
+            cnt[i]+=1
+       
+            
+q=deque()
+n,m=map(int,input().split())
+li=sorted(list(map(int,input().split())))
+arr,cnt=map(list,zip(*sorted(list(Counter(li).items()))))
+
+back(0,0)
