bf92901ee86a2619b7ffaf79bed4ea95907278b6
DEVHARAM <gkfka5968@naver.com>
2019-08-28 18:57:14 +0900

    1676-팩토리얼 0의 개수

---
+++ math/1676.py
@@ -0,0 +1,12 @@
+n=int(input())
+result=1
+for i in range(1,n+1):
+    result=result*i
+result=list(str(result))
+count=0
+for i in range(len(result)-1,0,-1):
+    if result[i]=='0':
+        count+=1
+    else:
+        break
+print(count)
